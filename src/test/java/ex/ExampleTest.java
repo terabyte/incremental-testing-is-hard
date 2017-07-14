bc62bc7d8f827a002e6c37f9237942b632514e2a
Carl Myers <cmyers@cmyers.org>
2017-07-14 14:10:17 -0700

    Writing stupid test

--- src/test/java/ex/ExampleTest.java
+++ src/test/java/ex/ExampleTest.java
@@ -1,5 +1,10 @@
 package ex;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.ClassLoader;
+import java.util.stream.Collectors;
+import org.junit.Assert;
 import org.junit.Test;
 
 import ex.Example;
@@ -9,5 +14,7 @@ public class ExampleTest {
     @Test
     public void someMethodTest() {
         Example.someMethod();
+        String result = new BufferedReader(new InputStreamReader(this.getClass().getClassLoader().getResourceAsStream("crap.txt"))).lines().collect(Collectors.joining("\n"));
+        Assert.assertEquals("correct", result);
     }
 }
