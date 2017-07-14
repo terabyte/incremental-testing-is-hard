6afb22f7f9cb8b5d5b358d1aaa72029f1afcb030
Carl Myers <cmyers@cmyers.org>
2017-07-14 13:58:06 -0700

    Initial commit

---
+++ src/main/java/ex/Example.java
@@ -0,0 +1,8 @@
+package ex;
+
+public class Example {
+
+    public static void someMethod() {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/test/java/ex/ExampleTest.java
@@ -0,0 +1,13 @@
+package ex;
+
+import org.junit.Test;
+
+import ex.Example;
+
+public class ExampleTest {
+
+    @Test
+    public void someMethodTest() {
+        Example.someMethod();
+    }
+}
